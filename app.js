614a7c5ad32d31a497d27bdaa6ecef8b035fcf21
DevTheTester <dev.kahika.campbell@gmail.com>
2025-06-17 21:52:08 +1200

    add posts and post index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const fs = require('fs');
 
 //Initializes an app object using express class
 const app = express();
@@ -15,8 +16,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layout'); //Looks for layout.ejs in views
 
-// Make the app use public folder to find images
-app.use(express.static('public'));
+// Set express layouts as the layouts we want to use? idek
 app.use(expressLayouts);
 
 //Routes
@@ -24,12 +24,26 @@ app.get('/', (req, res) => {
 	res.render('index'); 
 });
 
+
 app.get('/about', (req, res) => {
 	res.render('about'); 
 });
 
+// Turns the json into an html thingy? 
+const posts = JSON.parse(fs.readFileSync('./data/posts.json', 'utf-8');
+// Blog index page
 app.get('/blog', (req, res) => {
-	res.send('Welcome to the blog index, nothing here right now soz.');
+	res.render('blog', { posts }); 
+});
+
+// Single Blog Post View
+app.get('/blog/:slug', req, res => {
+	const post = posts.find(p -> p.slug === req.params.slug);
+	if (post) {
+		res.render('post', {post});
+	} else {
+		res.status(404).send('Post not found);
+	}
 });
 
 app.listen(port, () => {
